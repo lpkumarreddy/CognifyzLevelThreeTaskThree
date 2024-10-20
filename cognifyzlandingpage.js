80c6e4226753fa2cdfca82edf85473dda7bd347c
pavanreddy <144379629+lpkumarreddy@users.noreply.github.com>
2024-10-20 18:59:07 +0530

    Add files via upload

---
+++ cognifyzlandingpage.js
@@ -0,0 +1,633 @@
+let courseboX1Element = document.getElementById("courseboX1");
+
+function CourseBox1() {
+    courseboX1Element.style.backgroundColor = "#FFBF00";
+    courseboX1Element.style.borderColor = "darkblue";
+
+}
+let courseboX01Element = document.getElementById("courseboX01");
+
+function CourseBox01() {
+    courseboX01Element.style.backgroundColor = "#FFBF00";
+    courseboX01Element.style.borderColor = "darkblue";
+
+}
+let courseboX2Element = document.getElementById("courseboX2");
+
+function CourseBox2() {
+    courseboX2Element.style.backgroundColor = "#FFBF00";
+    courseboX2Element.style.borderColor = "darkblue";
+
+}
+let courseboX02Element = document.getElementById("courseboX02");
+
+function CourseBox02() {
+    courseboX02Element.style.backgroundColor = "#FFBF00";
+    courseboX02Element.style.borderColor = "darkblue";
+
+}
+let courseboX3Element = document.getElementById("courseboX3");
+
+function CourseBox3() {
+    courseboX3Element.style.backgroundColor = "#FFBF00";
+    courseboX3Element.style.borderColor = "darkblue";
+
+}
+let courseboX03Element = document.getElementById("courseboX03");
+
+function CourseBox03() {
+    courseboX03Element.style.backgroundColor = "#FFBF00";
+    courseboX03Element.style.borderColor = "darkblue";
+
+}
+let courseboX4Element = document.getElementById("courseboX4");
+
+function CourseBox4() {
+    courseboX4Element.style.backgroundColor = "#FFBF00";
+    courseboX4Element.style.borderColor = "darkblue";
+
+}
+let courseboX04Element = document.getElementById("courseboX04");
+
+function CourseBox04() {
+    courseboX04Element.style.backgroundColor = "#FFBF00";
+    courseboX04Element.style.borderColor = "darkblue";
+
+}
+let courseboX5Element = document.getElementById("courseboX5");
+
+function CourseBox5() {
+    courseboX5Element.style.backgroundColor = "#FFBF00";
+    courseboX5Element.style.borderColor = "darkblue";
+
+}
+let courseboX05Element = document.getElementById("courseboX05");
+
+function CourseBox05() {
+    courseboX05Element.style.backgroundColor = "#FFBF00";
+    courseboX05Element.style.borderColor = "darkblue";
+
+}
+let courseboX6Element = document.getElementById("courseboX6");
+
+function CourseBox6() {
+    courseboX6Element.style.backgroundColor = "#FFBF00";
+    courseboX6Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX06Element = document.getElementById("courseboX06");
+
+function CourseBox06() {
+    courseboX06Element.style.backgroundColor = "#FFBF00";
+    courseboX06Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX7Element = document.getElementById("courseboX7");
+
+function CourseBox7() {
+    courseboX7Element.style.backgroundColor = "#FFBF00";
+    courseboX7Element.style.borderColor = "darkblue";
+
+}
+let courseboX07Element = document.getElementById("courseboX07");
+
+function CourseBox07() {
+    courseboX07Element.style.backgroundColor = "#FFBF00";
+    courseboX07Element.style.borderColor = "darkblue";
+
+}
+let courseboX8Element = document.getElementById("courseboX8");
+
+function CourseBox8() {
+    courseboX8Element.style.backgroundColor = "#FFBF00";
+    courseboX8Element.style.borderColor = "darkblue";
+
+}
+let courseboX08Element = document.getElementById("courseboX08");
+
+function CourseBox08() {
+    courseboX08Element.style.backgroundColor = "#FFBF00";
+    courseboX08Element.style.borderColor = "darkblue";
+
+}
+let courseboX9Element = document.getElementById("courseboX9");
+
+function CourseBox9() {
+    courseboX9Element.style.backgroundColor = "#FFBF00";
+    courseboX9Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX09Element = document.getElementById("courseboX09");
+
+function CourseBox09() {
+    courseboX09Element.style.backgroundColor = "#FFBF00";
+    courseboX09Element.style.borderColor = "darkblue";
+
+}
+let courseboX10Element = document.getElementById("courseboX10");
+
+function CourseBox10() {
+    courseboX10Element.style.backgroundColor = "#FFBF00";
+    courseboX10Element.style.borderColor = "darkblue";
+
+}
+let courseboX010Element = document.getElementById("courseboX010");
+
+function CourseBox010() {
+    courseboX010Element.style.backgroundColor = "#FFBF00";
+    courseboX010Element.style.borderColor = "darkblue";
+
+}
+let courseboX11Element = document.getElementById("courseboX11");
+
+function CourseBox11() {
+    courseboX11Element.style.backgroundColor = "#FFBF00";
+    courseboX11Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX011Element = document.getElementById("courseboX011");
+
+function CourseBox011() {
+    courseboX011Element.style.backgroundColor = "#FFBF00";
+    courseboX011Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX12Element = document.getElementById("courseboX12");
+
+function CourseBox12() {
+    courseboX12Element.style.backgroundColor = "#FFBF00";
+    courseboX12Element.style.borderColor = "darkblue";
+
+}
+let courseboX012Element = document.getElementById("courseboX012");
+
+function CourseBox012() {
+    courseboX012Element.style.backgroundColor = "#FFBF00";
+    courseboX012Element.style.borderColor = "darkblue";
+
+}
+let courseboX13Element = document.getElementById("courseboX13");
+
+function CourseBox13() {
+    courseboX13Element.style.backgroundColor = "#FFBF00";
+    courseboX13Element.style.borderColor = "darkblue";
+
+}
+let courseboX013Element = document.getElementById("courseboX013");
+
+function CourseBox013() {
+    courseboX013Element.style.backgroundColor = "#FFBF00";
+    courseboX013Element.style.borderColor = "darkblue";
+
+}
+let courseboX14Element = document.getElementById("courseboX14");
+
+function CourseBox14() {
+    courseboX14Element.style.backgroundColor = "#FFBF00";
+    courseboX14Element.style.borderColor = "darkblue";
+
+}
+let courseboX014Element = document.getElementById("courseboX014");
+
+function CourseBox014() {
+    courseboX014Element.style.backgroundColor = "#FFBF00";
+    courseboX014Element.style.borderColor = "darkblue";
+
+}
+let courseboX15Element = document.getElementById("courseboX15");
+
+function CourseBox15() {
+    courseboX15Element.style.backgroundColor = "#FFBF00";
+    courseboX15Element.style.borderColor = "darkblue";
+
+}
+let courseboX015Element = document.getElementById("courseboX015");
+
+function CourseBox015() {
+    courseboX015Element.style.backgroundColor = "#FFBF00";
+    courseboX015Element.style.borderColor = "darkblue";
+
+}
+let courseboX16Element = document.getElementById("courseboX16");
+
+function CourseBox16() {
+    courseboX16Element.style.backgroundColor = "#FFBF00";
+    courseboX16Element.style.borderColor = "darkblue";
+
+}
+let courseboX016Element = document.getElementById("courseboX016");
+
+function CourseBox016() {
+    courseboX016Element.style.backgroundColor = "#FFBF00";
+    courseboX016Element.style.borderColor = "darkblue";
+
+}
+let courseboX17Element = document.getElementById("courseboX17");
+
+function CourseBox17() {
+    courseboX17Element.style.backgroundColor = "#FFBF00";
+    courseboX17Element.style.borderColor = "darkblue";
+
+}
+let courseboX017Element = document.getElementById("courseboX017");
+
+function CourseBox017() {
+    courseboX017Element.style.backgroundColor = "#FFBF00";
+    courseboX017Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX18Element = document.getElementById("courseboX18");
+
+function CourseBox18() {
+    courseboX18Element.style.backgroundColor = "#FFBF00";
+    courseboX18Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX018Element = document.getElementById("courseboX018");
+
+function CourseBox018() {
+    courseboX018Element.style.backgroundColor = "#FFBF00";
+    courseboX018Element.style.borderColor = "darkblue";
+
+}
+let courseboX19Element = document.getElementById("courseboX19");
+
+function CourseBox19() {
+    courseboX19Element.style.backgroundColor = "#FFBF00";
+    courseboX19Element.style.borderColor = "darkblue";
+
+}
+let courseboX019Element = document.getElementById("courseboX019");
+
+function CourseBox019() {
+    courseboX019Element.style.backgroundColor = "#FFBF00";
+    courseboX019Element.style.borderColor = "darkblue";
+
+}
+let courseboX20Element = document.getElementById("courseboX20");
+
+function CourseBox20() {
+    courseboX20Element.style.backgroundColor = "#FFBF00";
+    courseboX20Element.style.borderColor = "darkblue";
+
+}
+let courseboX020Element = document.getElementById("courseboX020");
+
+function CourseBox020() {
+    courseboX020Element.style.backgroundColor = "#FFBF00";
+    courseboX020Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX21Element = document.getElementById("courseboX21");
+
+function CourseBox21() {
+    courseboX21Element.style.backgroundColor = "#FFBF00";
+    courseboX21Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX021Element = document.getElementById("courseboX021");
+
+function CourseBox021() {
+    courseboX021Element.style.backgroundColor = "#FFBF00";
+    courseboX021Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX22Element = document.getElementById("courseboX22");
+
+function CourseBox22() {
+    courseboX22Element.style.backgroundColor = "#FFBF00";
+    courseboX22Element.style.borderColor = "darkblue";
+
+}
+let courseboX022Element = document.getElementById("courseboX022");
+
+function CourseBox022() {
+    courseboX022Element.style.backgroundColor = "#FFBF00";
+    courseboX022Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX23Element = document.getElementById("courseboX23");
+
+function CourseBox23() {
+    courseboX23Element.style.backgroundColor = "#FFBF00";
+    courseboX23Element.style.borderColor = "darkblue";
+
+}
+let courseboX023Element = document.getElementById("courseboX023");
+
+function CourseBox023() {
+    courseboX023Element.style.backgroundColor = "#FFBF00";
+    courseboX023Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX24Element = document.getElementById("courseboX24");
+
+function CourseBox24() {
+    courseboX24Element.style.backgroundColor = "#FFBF00";
+    courseboX24Element.style.borderColor = "darkblue";
+
+}
+let courseboX024Element = document.getElementById("courseboX024");
+
+function CourseBox024() {
+    courseboX024Element.style.backgroundColor = "#FFBF00";
+    courseboX024Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX25Element = document.getElementById("courseboX25");
+
+function CourseBox25() {
+    courseboX25Element.style.backgroundColor = "#FFBF00";
+    courseboX25Element.style.borderColor = "darkblue";
+
+}
+let courseboX025Element = document.getElementById("courseboX025");
+
+function CourseBox025() {
+    courseboX025Element.style.backgroundColor = "#FFBF00";
+    courseboX025Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX26Element = document.getElementById("courseboX26");
+
+function CourseBox26() {
+    courseboX26Element.style.backgroundColor = "#FFBF00";
+    courseboX26Element.style.borderColor = "darkblue";
+
+}
+let courseboX026Element = document.getElementById("courseboX026");
+
+function CourseBox026() {
+    courseboX026Element.style.backgroundColor = "#FFBF00";
+    courseboX026Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX27Element = document.getElementById("courseboX27");
+
+function CourseBox27() {
+    courseboX27Element.style.backgroundColor = "#FFBF00";
+    courseboX27Element.style.borderColor = "darkblue";
+
+}
+let courseboX027Element = document.getElementById("courseboX027");
+
+function CourseBox027() {
+    courseboX027Element.style.backgroundColor = "#FFBF00";
+    courseboX027Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX28Element = document.getElementById("courseboX28");
+
+function CourseBox28() {
+    courseboX28Element.style.backgroundColor = "#FFBF00";
+    courseboX28Element.style.borderColor = "darkblue";
+
+}
+let courseboX028Element = document.getElementById("courseboX028");
+
+function CourseBox028() {
+    courseboX028Element.style.backgroundColor = "#FFBF00";
+    courseboX028Element.style.borderColor = "darkblue";
+
+}
+
+
+let courseboX29Element = document.getElementById("courseboX29");
+
+function CourseBox29() {
+    courseboX29Element.style.backgroundColor = "#FFBF00";
+    courseboX29Element.style.borderColor = "darkblue";
+
+}
+let courseboX029Element = document.getElementById("courseboX029");
+
+function CourseBox029() {
+    courseboX029Element.style.backgroundColor = "#FFBF00";
+    courseboX029Element.style.borderColor = "darkblue";
+
+}
+
+
+let courseboX30Element = document.getElementById("courseboX30");
+
+function CourseBox30() {
+    courseboX30Element.style.backgroundColor = "#FFBF00";
+    courseboX30Element.style.borderColor = "darkblue";
+
+}
+let courseboX030Element = document.getElementById("courseboX030");
+
+function CourseBox030() {
+    courseboX030Element.style.backgroundColor = "#FFBF00";
+    courseboX030Element.style.borderColor = "darkblue";
+
+}
+
+let courseboX31Element = document.getElementById("courseboX31");
+
+function CourseBox31() {
+    courseboX31Element.style.backgroundColor = "#1c8e00";
+    courseboX31Element.style.borderColor = "#000";
+    courseboX31Element.style.color = "#fff";
+}
+let courseboX031Element = document.getElementById("courseboX031");
+
+function CourseBox031() {
+    courseboX031Element.style.backgroundColor = "#1c8e00";
+    courseboX031Element.style.borderColor = "#000";
+    courseboX031Element.style.color = "#fff";
+}
+
+let courseboX32Element = document.getElementById("courseboX32");
+
+function CourseBox32() {
+    courseboX32Element.style.backgroundColor = "#6009b2";
+    courseboX32Element.style.borderColor = "#000";
+    courseboX32Element.style.color = "#fff";
+}
+let courseboX032Element = document.getElementById("courseboX032");
+
+function CourseBox032() {
+    courseboX032Element.style.backgroundColor = "#6009b2";
+    courseboX032Element.style.borderColor = "#000";
+    courseboX032Element.style.color = "#fff";
+
+}
+let courseboX33Element = document.getElementById("courseboX33");
+
+function CourseBox33() {
+    courseboX33Element.style.backgroundColor = "#06c220";
+    courseboX33Element.style.borderColor = "#000";
+    courseboX33Element.style.color = "#fff";
+
+}
+let courseboX033Element = document.getElementById("courseboX033");
+
+function CourseBox033() {
+    courseboX033Element.style.backgroundColor = "#06c220";
+    courseboX033Element.style.borderColor = "#000";
+    courseboX033Element.style.color = "#fff";
+
+}
+let courseboX34Element = document.getElementById("courseboX34");
+
+function CourseBox34() {
+    courseboX34Element.style.backgroundColor = "#a60909";
+    courseboX34Element.style.borderColor = "#000";
+    courseboX34Element.style.color = "#fff";
+
+}
+let courseboX034Element = document.getElementById("courseboX034");
+
+function CourseBox034() {
+    courseboX034Element.style.backgroundColor = "#a60909";
+    courseboX034Element.style.borderColor = "#000";
+    courseboX034Element.style.color = "#fff";
+
+}
+let courseboX35Element = document.getElementById("courseboX35");
+
+function CourseBox35() {
+    courseboX35Element.style.backgroundColor = "#05b4c2";
+    courseboX35Element.style.borderColor = "#000";
+    courseboX35Element.style.color = "#fff";
+
+}
+let courseboX035Element = document.getElementById("courseboX035");
+
+function CourseBox035() {
+    courseboX035Element.style.backgroundColor = "#05b4c2";
+    courseboX035Element.style.borderColor = "#000";
+    courseboX035Element.style.color = "#fff";
+
+}
+let courseboX36Element = document.getElementById("courseboX36");
+
+function CourseBox36() {
+    courseboX36Element.style.backgroundColor = "#e4ee08";
+    courseboX36Element.style.borderColor = "#000";
+    courseboX36Element.style.color = "#fff";
+
+}
+let courseboX036Element = document.getElementById("courseboX036");
+
+function CourseBox036() {
+    courseboX036Element.style.backgroundColor = "#e4ee08";
+    courseboX036Element.style.borderColor = "#000";
+    courseboX036Element.style.color = "#000";
+
+}
+let courseboX37Element = document.getElementById("courseboX37");
+
+function CourseBox37() {
+    courseboX37Element.style.backgroundColor = "#FFBF00";
+    courseboX37Element.style.borderColor = "#000";
+    courseboX37Element.style.color = "#fff";
+
+}
+let courseboX037Element = document.getElementById("courseboX037");
+
+function CourseBox037() {
+    courseboX037Element.style.backgroundColor = "#FFBF00";
+    courseboX037Element.style.borderColor = "#000";
+    courseboX037Element.style.color = "#fff";
+
+}
+let courseboX38Element = document.getElementById("courseboX38");
+
+function CourseBox38() {
+    courseboX38Element.style.backgroundColor = "#FFBF00";
+    courseboX38Element.style.borderColor = "#000";
+    courseboX38Element.style.color = "#fff";
+
+}
+let courseboX038Element = document.getElementById("courseboX038");
+
+function CourseBox038() {
+    courseboX038Element.style.backgroundColor = "#FFBF00";
+    courseboX038Element.style.borderColor = "#000";
+    courseboX038Element.style.color = "#fff";
+
+}
+let courseboX39Element = document.getElementById("courseboX39");
+
+function CourseBox39() {
+    courseboX39Element.style.backgroundColor = "#FFBF00";
+    courseboX39Element.style.borderColor = "#000";
+    courseboX39Element.style.color = "#fff";
+
+}
+let courseboX039Element = document.getElementById("courseboX039");
+
+function CourseBox039() {
+    courseboX039Element.style.backgroundColor = "#FFBF00";
+    courseboX039Element.style.borderColor = "#000";
+    courseboX039Element.style.color = "#fff";
+
+}
+let courseboX40Element = document.getElementById("courseboX40");
+
+function CourseBox40() {
+    courseboX40Element.style.backgroundColor = "#FFBF00";
+    courseboX40Element.style.borderColor = "#000";
+    courseboX40Element.style.color = "#fff";
+
+}
+let courseboX040Element = document.getElementById("courseboX040");
+
+function CourseBox040() {
+    courseboX040Element.style.backgroundColor = "#FFBF00";
+    courseboX040Element.style.borderColor = "#000";
+    courseboX040Element.style.color = "#fff";
+
+}
+
+let courseboX41Element = document.getElementById("courseboX41");
+
+function CourseBox41() {
+    courseboX41Element.style.backgroundColor = "#FFBF00";
+    courseboX41Element.style.borderColor = "#000";
+    courseboX41Element.style.color = "#fff";
+
+}
+let courseboX041Element = document.getElementById("courseboX041");
+
+function CourseBox041() {
+    courseboX041Element.style.backgroundColor = "#FFBF00";
+    courseboX041Element.style.borderColor = "#000";
+    courseboX041Element.style.color = "#fff";
+
+}
+
+let courseboX42Element = document.getElementById("courseboX42");
+
+function CourseBox42() {
+    courseboX42Element.style.backgroundColor = "#FFBF00";
+    courseboX42Element.style.borderColor = "#000";
+    courseboX42Element.style.color = "#fff";
+
+}
+let courseboX042Element = document.getElementById("courseboX042");
+
+function CourseBox042() {
+    courseboX042Element.style.backgroundColor = "#FFBF00";
+    courseboX042Element.style.borderColor = "#000";
+    courseboX042Element.style.color = "#fff";
+
+}
